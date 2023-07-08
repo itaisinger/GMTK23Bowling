b04e6b888e82e9edbc9a1f97e4a2ab51426bbfa4
itaisinger <itai5000si2@gmail.com>
2023-07-08 04:31:21 +0300

    pins and pushbacks done

--- GMTK23Bowling/Assets/Scripts/PinScript.cs
+++ GMTK23Bowling/Assets/Scripts/PinScript.cs
@@ -24,16 +24,26 @@ public class PinScript : MonoBehaviour
             bounceCooldown -= Time.deltaTime;   
     }
 
-    private void OnTriggerEnter2D(Collider2D other) 
+    //pushback
+    private void OnTriggerStay2D(Collider2D other) 
     {
+
+      if(other.tag == "PushBack")
+      {
+        float baseForce = other.GetComponent<PushBackScript>().force;
+        float angleDegrees = other.GetComponent<PushBackScript>().angle;
+        float angleRadians = angleDegrees * Mathf.Deg2Rad;
+        var forceMult = Mathf.Max(1f, rb.velocity.magnitude);
+
+        Vector2 vec = baseForce * forceMult * new Vector2(Mathf.Sin(angleRadians), Mathf.Cos(angleRadians));
+
+        rb.AddForce(vec);
+      }
+
       //SCARPPED!
       return;
       if(other.gameObject.CompareTag("Player")&& bounceCooldown <= 0f)
       {
-          var mouseVector = other.gameObject.GetComponent<MouseContoller>().GetPushVector();
-          rb.AddForce(mouseVector*force,ForceMode2D.Impulse);
-          bounceCooldown = bounceCooldownMax;
-          Debug.Log("Force :" + mouseVector.ToString());
           //niv code
           // Vector2 targetDir = transform.position - other.gameObject.GetComponent<Transform>().position;
           // rb.AddForce(targetDir*force,ForceMode2D.Impulse);
@@ -41,6 +51,7 @@ public class PinScript : MonoBehaviour
       }    
     }
 
+    //die
     private void OnCollisionEnter2D(Collision2D other) 
     {
       if(other.gameObject.CompareTag("Ball"))
---
+++ GMTK23Bowling/Assets/Scripts/PushBackScript.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushBackScript : MonoBehaviour
+{
+    [SerializeField] public float force = 200f;
+    [SerializeField] public float angle = 0f;
+}
