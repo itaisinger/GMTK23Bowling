608ba828222f816241e4e4bf9da6673cd2a86d51
itaisinger <itai5000si2@gmail.com>
2023-07-08 04:34:08 +0300

    cleaned up code

--- GMTK23Bowling/Assets/Scripts/MouseContoller.cs
+++ GMTK23Bowling/Assets/Scripts/MouseContoller.cs
@@ -5,37 +5,12 @@ using UnityEngine;
 public class MouseContoller : MonoBehaviour
 {
     [SerializeField] Dictionary<GameObject,bool> collisions = new Dictionary<GameObject, bool>();     
-    
-    //unused
-    int stackSize=10;
-    List<Vector2> vectors = new List<Vector2>();
-    Vector3 lastPos;
-
-    void Awake()
-    {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = Camera.main.transform.position.z + Camera.main.nearClipPlane;
-        lastPos = transform.position;
-
-        vectors.Add(lastPos);
-    }
 
     void FixedUpdate()
     {
-        //// unused ////
-
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = Camera.main.transform.position.z + Camera.main.nearClipPlane;
         transform.position = mousePosition;
-
-        //add vector to stack
-        Vector2 newVec = mousePosition - lastPos;
-        lastPos = mousePosition;
-        vectors.Add(newVec);
-
-        //pop
-        if(vectors.Count > stackSize)
-            vectors.RemoveAt(0);
     }
 
     void Update()
@@ -59,25 +34,8 @@ public class MouseContoller : MonoBehaviour
                 float dirRadians = dirDegrees * Mathf.Deg2Rad;
                 Vector2 vec = (1/1) * pinScript.force * new Vector2(Mathf.Sin(dirRadians), Mathf.Cos(dirRadians));
                 pinBody.AddForce(vec);
-                Debug.Log(dirDegrees);
             }
         }
-    }
-
-    //unused
-    public Vector2 GetPushVector()
-    {
-        Vector2 ret = new Vector2(0f,0f);
-
-        //loop through and sum all the vectors
-        for(var i=0; i < vectors.Count; i++)
-        {
-            ret += vectors[i];
-        }
-
-        ret /= vectors.Count;
-
-        return ret;
     }
 
     //add pin to collision
@@ -86,19 +44,11 @@ public class MouseContoller : MonoBehaviour
         if(col.tag == "Pin")
             collisions.Add(col.gameObject, true);
     }
+    //remove pin from collisions
     void OnTriggerExit2D(Collider2D col)
     {
         if(col.tag == "Pin")
             collisions.Remove(col.gameObject); 
-    }
-    void OnTriggerStay2D(Collider2D col)
-    {
-        if(col.tag == "Pin")
-        {
-            var vec = transform.position - col.transform.position;
-            Debug.Log(FindDegree(vec));
-        }
-
     }
 
     private float FindDegree(Vector2 vector){
