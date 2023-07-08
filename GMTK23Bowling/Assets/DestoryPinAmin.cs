4039e8160255ea046851ad8fb92633bb0ae5e1bf
NivGarbelly <60030777+NivGarbelly@users.noreply.github.com>
2023-07-08 16:18:00 +0300

    .

---
+++ GMTK23Bowling/Assets/DestoryPinAmin.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryPinAmin : MonoBehaviour
+{
+    public void DestoryPin()
+    {
+        Destroy(this.gameObject);
+    }
+}
